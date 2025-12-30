f14ea2115d1cc73358fc44cd2ef1733b95a1453a
SaiDeepthi27 <ravikantisaideepthi27@gmail.com>
2025-12-30 14:14:51 +0530

    Maven updates

---
+++ Maven_projects/ReverseOfNumber/src/main/java/com/skillnext2/App.java
@@ -0,0 +1,17 @@
+package com.skillnext2;
+import java.util.Scanner;
+public class App {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+        int reversed = 0;
+        while (num != 0) {
+            int digit = num % 10;
+            reversed = reversed * 10 + digit;
+            num = num / 10;
+        }
+        System.out.println("Reversed number is: " + reversed);
+    }
+}
+
